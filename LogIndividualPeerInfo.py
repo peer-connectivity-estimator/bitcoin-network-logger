0fbf066b57fcc255aecbddecf9e52792941831f4
Simeon Wuthier <simeonwuthier@gmail.com>
2023-07-31 22:39:03 -0600

    Add print statements for improved clarity

--- LogIndividualPeerInfo.py
+++ LogIndividualPeerInfo.py
@@ -46,12 +46,16 @@ if os.path.exists(f'/media/{user}/BTC'):
 	outputFilesToTransferPath = f'/media/{user}/BTC/Official_Research_Logs'
 elif os.path.exists('/media/sf_Shared_Folder'):
 	outputFilesToTransferPath = '/media/sf_Shared_Folder/Official_Research_Logs'
+print(f'Path to send finalized outputs: "{outputFilesToTransferPath}"')
 
 # The path where the Bitcoin blockchain is stored
 bitcoinDirectory = f'/home/{user}/.bitcoin'
 if os.path.exists(f'/home/{user}/BitcoinFullLedger'):
 	numSamplesPerDirectory = 10000 # 10000*10/60/60 = 27.7 hours
 	bitcoinDirectory = f'/home/{user}/BitcoinFullLedger'
+print(f'Bitcoin directory path: "{outputFilesToTransferPath}"')
+print(f'Number of seconds per sample: {numSecondsPerSample}')
+print(f'Number of samples per directory: {numSamplesPerDirectory}')
 
 
 # Keep three decimal points for timestamps and time durations
