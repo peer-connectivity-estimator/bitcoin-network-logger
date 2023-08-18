55daef5f5671120e5e4d0e1b6c8137e41dab8b26
Simeon Wuthier <simeonwuthier@gmail.com>
2023-08-17 19:46:24 -0600

    Remove unused configuration

--- src/init.cpp
+++ src/init.cpp
@@ -503,9 +503,8 @@ void SetupServerArgs(ArgsManager& argsman)
     argsman.AddArg("-peertimeout=<n>", strprintf("Specify a p2p connection timeout delay in seconds. After connecting to a peer, wait this amount of time before considering disconnection based on inactivity (minimum: 1, default: %d)", DEFAULT_PEER_CONNECT_TIMEOUT), ArgsManager::ALLOW_ANY | ArgsManager::DEBUG_ONLY, OptionsCategory::CONNECTION);
     argsman.AddArg("-torcontrol=<ip>:<port>", strprintf("Tor control port to use if onion listening enabled (default: %s)", DEFAULT_TOR_CONTROL), ArgsManager::ALLOW_ANY, OptionsCategory::CONNECTION);
     argsman.AddArg("-torpassword=<pass>", "Tor control port password (default: empty)", ArgsManager::ALLOW_ANY | ArgsManager::SENSITIVE, OptionsCategory::CONNECTION);
-    // Cybersecurity Lab: Additional minconnections and donotlogtoconsole configuration parameters
-    argsman.AddArg("-minconnections=<n>", strprintf("Maintain <n> connections to peers (default: %u)", -1), ArgsManager::ALLOW_ANY, OptionsCategory::CONNECTION); // Cybersecurity Lab
-    argsman.AddArg("-donotlogtoconsole", strprintf("Prevent print statements from showing up in the console; but still show in the debug.log file (default: %d)", false), ArgsManager::ALLOW_ANY, OptionsCategory::CONNECTION);
+    // Cybersecurity Lab: Additional minconnections configuration parameters
+    argsman.AddArg("-minconnections=<n>", strprintf("Maintain <n> connections to peers (default: %u)", -1), ArgsManager::ALLOW_ANY, OptionsCategory::CONNECTION);
 #ifdef USE_UPNP
 #if USE_UPNP
     argsman.AddArg("-upnp", "Use UPnP to map the listening port (default: 1 when listening and no -proxy)", ArgsManager::ALLOW_ANY, OptionsCategory::CONNECTION);
