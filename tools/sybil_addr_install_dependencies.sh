7b9e2323f5445d7851ce10ce23a355f86a7ab35e
simewu <swuthier@uccs.edu>
2024-01-26 11:59:14 -0700

    Add addr sending script dependencies

---
+++ tools/sybil_addr_install_dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install python-bitcoinlib for Bitcoin-related functionality
+python3 -m pip install python-bitcoinlib
+
+# Ensure you have the latest pip and setuptools
+python3 -m pip install --upgrade pip setuptools
