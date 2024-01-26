427f59b9e661ff8946c0d9f8f903f3731280dd5a
simewu <swuthier@uccs.edu>
2024-01-26 12:03:06 -0700

    Add addr sending script dependencies

--- tools/sybil_addr_install_dependencies.sh
+++ tools/sybil_addr_install_dependencies.sh
@@ -3,5 +3,8 @@
 # Install python-bitcoinlib for Bitcoin-related functionality
 python3 -m pip install python-bitcoinlib
 
+# Attempt to install a package named 'bitcoin' if it exists
+python3 -m pip install bitcoin
+
 # Ensure you have the latest pip and setuptools
 python3 -m pip install --upgrade pip setuptools
