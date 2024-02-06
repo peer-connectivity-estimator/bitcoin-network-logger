7b012adecba3efbd3b9ee8922730c02bb29a0596
Simeon Wuthier <simeonwuthier@gmail.com>
2024-02-06 14:23:58 -0700

    Fix iptables modifications

--- tools/clear_iptables.sh
+++ tools/clear_iptables.sh
@@ -1,12 +1,26 @@
+#!/bin/bash
+
+# Zero the packet and byte counters in all chains
 sudo iptables -Z
 
+# Flush all rules in the filter table
 sudo iptables -F
 
+# Set the default policies for the filter table
 sudo iptables -P INPUT ACCEPT
 sudo iptables -P FORWARD ACCEPT
 sudo iptables -P OUTPUT ACCEPT
 
+# Flush all rules in the nat table
 sudo iptables -t nat -F
+
+# Flush all rules in the mangle table
 sudo iptables -t mangle -F
+
+# Flush all rules again to catch any that might have been missed
 sudo iptables -F
+
+# Delete all user-defined chains
 sudo iptables -X
+
+echo "iptables has been reset to default state."
--- tools/iptables_only_allow_local_connections.sh
+++ tools/iptables_only_allow_local_connections.sh
@@ -1,3 +1,30 @@
+#!/bin/bash
+
+# Display network interfaces
+ifconfig
+
+# Ask the user to confirm the network interface
+echo "Please enter the network interface from the above list that you wish to configure (default is enp0s3):"
+read interface
+
+./clear_iptables.sh
+
+# Set default interface if none specified
+if [ -z "$interface" ]; then
+	interface="enp0s3"
+fi
+
+echo "Configuring iptables for interface: $interface"
+
+# iptables configuration
 sudo iptables -P FORWARD DROP # We aren't a router
-sudo iptables -A INPUT -i enp0s3 -m iprange --src-range 10.0.2.1-10.0.2.255 -j ACCEPT
+sudo iptables -A INPUT -i "$interface" -m iprange --src-range 10.0.2.1-10.0.2.255 -j ACCEPT
+
+# Accept all incoming and outgoing traffic to and from localhost (127.0.0.1)
+sudo iptables -A INPUT -i lo -j ACCEPT
+sudo iptables -A OUTPUT -o lo -j ACCEPT
+
 sudo iptables -P INPUT DROP # Drop everything we don't accept
+sudo iptables -P OUTPUT ACCEPT # Default OUTPUT policy (adjust if needed)
+
+echo "iptables configuration updated."
